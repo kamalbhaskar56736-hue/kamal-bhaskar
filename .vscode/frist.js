8d06fbf8d3b283e03967fe14ddbe36467ee01f1c
Kamal Bhaskar <kamalbhaskar56736@gmail.com>
2025-11-22 21:47:26 +0530

    delete one file

---
+++ .vscode/frist.js
@@ -0,0 +1,8 @@
+let age = 18;
+
+if (age>=10) {
+    console.log("vote");
+}else{
+    console.log("not vote")
+    
+}
---
+++ p.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(){
+int i=0;
+while (1<10){
+    printf("The value of is %d\n",i);
+    i++;
+}
+return 0;
+}
